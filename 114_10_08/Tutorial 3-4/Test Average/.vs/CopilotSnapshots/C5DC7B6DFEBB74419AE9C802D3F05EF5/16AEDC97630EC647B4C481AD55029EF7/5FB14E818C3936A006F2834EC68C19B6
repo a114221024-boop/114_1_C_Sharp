4e9fc9153e4387951c9571f5cc497215ef464401
a114221024-boop <a114221024@mail.shu.edu.tw>
2025-10-08 12:58:03 +0800

    114_10_08

---
+++ 114_10_08/Tutorial 3-4/Test Average/.vs/CopilotSnapshots/C5DC7B6DFEBB74419AE9C802D3F05EF5/16AEDC97630EC647B4C481AD55029EF7/5FB14E818C3936A006F2834EC68C19B6
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_Average
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double test1, test2, test3;
+            // 取得三個分數並計算平均
+            if (double.TryParse(test1TextBox.Text, out test1) &&
+                double.TryParse(test2TextBox.Text, out test2) &&
+                double.TryParse(test3TextBox.Text, out test3))
+            {
+                double average = (test1 + test2 + test3) / 3;
+                averageLabel.Text = average.ToString("n2");
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的分數！");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+            averageLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ 114_10_08/Tutorial 3-5/Change Counter/Change Counter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Counter
+{
+    public partial class Form1 : Form
+    {
+        const decimal FIVE_CENT = 5.0m;
+        const decimal TEN_CENT = 10.0m;
+        const decimal TWENTY_FIVE_CENT = 25.0m;
+        const decimal FIFTY_CENT = 50.0m;
+        private decimal total;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void fiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 5;
+            totalLabel.Text = total.ToString();
+        }
+
+        private void tenCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 10;
+            totalLabel.Text = total.ToString();
+        }
+
+        private void twentyFiveCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 25;
+            totalLabel.Text = total.ToString();
+        }
+
+        private void fiftyCentsPictureBox_Click(object sender, EventArgs e)
+        {
+            total += 50;
+            totalLabel.Text = total.ToString();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateTotalLabel()
+        {
+            totalLabel.Text = total.ToString("C2");
+        }
+    }
+}
